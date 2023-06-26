c49d78b09a2af7617196101fed3c2e6246d130ff
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2023-06-26 10:08:57 -0300

    Update object_tracking.py

--- object_tracking.py
+++ object_tracking.py
@@ -19,18 +19,11 @@ tracker.init(img, bbox)
 print(bbox)
 
 def drawBox(img, bbox):
-    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-
-    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
-
-    cv2.putText(img,"Rastreando",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
-
-def goal_track(img, bbox):
-    x, y,w, h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
-
     ##########################
     # ADICIONE O CÓDIGO AQUI #
     ##########################
+
+
 
 while True:
     
@@ -43,10 +36,6 @@ while True:
         drawBox(img, bbox)
     else:
         cv2.putText(img,"Errou",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-
-    ##########################
-    # ADICIONE O CÓDIGO AQUI #
-    ##########################
 
     cv2.imshow("resultado", img)
             
